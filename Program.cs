8a5364f5b97f807e46f3ec5607bbe403a50d2cbb
rhinod <isc.damians@gmail.com>
2022-10-11 09:11:13 -0700

    Add initial project files

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TechnicalInterview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\input.txt";
+            var lines = File.ReadAllLines(path);
+            Array.ForEach(lines, line => Console.WriteLine(line));
+            Console.ReadKey();
+        }
+    }
+}
